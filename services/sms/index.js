8588f3aea22da4c85e90ce71eac9dce36dd6055e
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-10-29 15:01:42 +0100

    add sms service

--- services/sms/index.js
+++ services/sms/index.js
@@ -1,1 +1,7 @@
-//SMS
+const Vonage = require('@vonage/server-sdk')
+const vonage = new Vonage({
+  apiKey: "19d94037",
+  apiSecret: "MxgoD1RhPcPn1tpv"
+})
+
+sendCustomSms("243825937168", "Tuna Biso, plateforme de lutte contre les fake news")
