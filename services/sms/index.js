f1b0a4b076dadc9433b068a11225d25dfb454e1b
DMiyalu <dieudonnemiyalu6@gmail.com>
2021-10-29 15:34:00 +0100

    send sms

--- services/sms/index.js
+++ services/sms/index.js
@@ -3,5 +3,18 @@ const vonage = new Vonage({
   apiKey: "19d94037",
   apiSecret: "MxgoD1RhPcPn1tpv"
 })
-
+const from = "Tuna Biso"
+const sendCustomSms = (to, text) => {
+    vonage.message.sendSms(from, to, text, (err, responseData) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if(responseData.messages[0]['status'] === "0") {
+                console.log("Message sent successfully.");
+            } else {
+                console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+            }
+        }
+    })
+}
 sendCustomSms("243825937168", "Tuna Biso, plateforme de lutte contre les fake news")
